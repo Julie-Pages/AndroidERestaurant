38ec9ca19230454209f323bf5d1bd83bc4955afa
Julie-Pages <julie.pages@isen.yncrea.fr>
2022-01-24 16:38:55 +0100

    ajout bouton entree

--- app/src/main/java/fr/isen/pages/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/pages/androiderestaurant/HomeActivity.kt
@@ -1,5 +1,6 @@
 package fr.isen.pages.androiderestaurant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +9,12 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val entreeButton =findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.button2)
+        entreeButton.setOnClickListener {
+
+            val Intent = Intent(this,EntreeActivity::class.java)
+            startActivity(Intent)
+        }
     }
 }
