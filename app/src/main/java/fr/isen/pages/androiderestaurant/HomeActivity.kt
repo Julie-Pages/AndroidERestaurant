7b94c90faae0e1e02cc86f41bdd9ebf8482e140e
Julie-Pages <julie.pages@isen.yncrea.fr>
2022-01-25 14:37:13 +0100

    RecyclerView

--- app/src/main/java/fr/isen/pages/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/pages/androiderestaurant/HomeActivity.kt
@@ -1,20 +1,23 @@
 package fr.isen.pages.androiderestaurant
 
+
 import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import android.util.Log
 
+
 class HomeActivity : AppCompatActivity() {
-
-
-
 
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+//button working
 
         val starterButton =findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.btnStarter)
         val dishButton =findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.btnDish)
@@ -48,6 +51,30 @@ class HomeActivity : AppCompatActivity() {
             changeActivity(getString(R.string.home_dessert))
 
         }
+
+
+//RecyclerView
+        // getting the recyclerview by its id
+        val recyclerview = findViewById<RecyclerView>(R.id.rvStarter)
+
+        // this creates a vertical layout Manager
+        recyclerview.layoutManager = LinearLayoutManager(this)
+
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<ItemsViewModel>()
+
+        // This loop will create 20 Views containing
+        // the image with the count of view
+        for (i in 1..20) {
+            data.add(ItemsViewModel(R.string.home_starter1, "Item " + i))
+        }
+
+        // This will pass the ArrayList to our Adapter
+        val adapter = CustomAdapter(data)
+
+        // Setting the Adapter with the recyclerview
+        recyclerview.adapter = adapter
+
     }
 
     private fun changeActivity( category: String ) {
@@ -56,4 +83,9 @@ class HomeActivity : AppCompatActivity() {
         startActivity(Intent)
 
     }
+
+
+
 }
+
+
---
+++ app/src/main/java/fr/isen/pages/androiderestaurant/ItemsViewModel.kt
@@ -0,0 +1,4 @@
+package fr.isen.pages.androiderestaurant
+
+data class ItemsViewModel(val image: Int, val text: String) {
+}
